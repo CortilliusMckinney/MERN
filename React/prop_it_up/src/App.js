9c1ef33112223e1c7103305cf228f7c0670f1927
Cortillius Mckinney <cortilliusmckinney@gmail.com>
2022-10-01 19:46:41 -0400

    FinalCommit

--- React/prop_it_up/src/App.js
+++ React/prop_it_up/src/App.js
@@ -5,21 +5,34 @@ import PersonCard from "./components/PersonCard";
 function App(props) {
   return (
     <div className="App">
-      <PersonCard lastName={"Doe,"} firstName={"Jane"} />
-      <PersonCard age={"Age: 45"} />
-      <PersonCard hairColor={"Hair Color: Black"} />
+      <PersonCard
+        lastName={"Doe,"}
+        firstName={"Jane"}
+        age={"Age: 45"}
+        hairColor={"Hair Color: Black"}
+      />
+      <PersonCard
+        lastName={"John,"}
+        firstName={"Smith"}
+        age={"Age: 88"}
+        hairColor={"Hair Color: Brown"}
+      />
 
-      <PersonCard lastName={"John,"} firstName={"Smith"} />
-      <PersonCard age={"Age: 88"} />
-      <PersonCard hairColor={"Hair Color: Brown"} />
+      <PersonCard
+        lastName={"Fillmore,"}
+        firstName={"Millard"}
+        age={"Age: 50"}
+        PersonCard
+        hairColor={"Hair Color: Brown"}
+      />
 
-      <PersonCard lastName={"Fillmore,"} firstName={"Millard"} />
-      <PersonCard age={"Age: 50"} />
-      <PersonCard hairColor={"Hair Color: Brown"} />
-
-      <PersonCard lastName={"Smith,"} firstName={"Maria"} />
-      <PersonCard age={"Age: 50"} />
-      <PersonCard hairColor={"Hair Color: Brown"} />
+      <PersonCard
+        lastName={"Smith,"}
+        firstName={"Maria"}
+        age={"Age: 34"}
+        PersonCard
+        hairColor={"Hair Color: Brown"}
+      />
     </div>
   );
 }
